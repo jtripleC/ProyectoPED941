67b9a2493d9bccae78c16dcddd80478036329a15
Carlos Ernesto Contreras Bonilla <CB121033@alumno.udb.edu.sv>
2026-04-13 22:25:48 -0600

    Creaciön de la estructura básica del sistema (models, forms, data, structure, services)

--- SolucionProyecto_PED941/Program.cs
+++ SolucionProyecto_PED941/Program.cs
@@ -1,3 +1,5 @@
+using SolucionProyecto_PED941.Forms;
+
 namespace SolucionProyecto_PED941
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace SolucionProyecto_PED941
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
